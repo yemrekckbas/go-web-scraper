4cea990d765efe43d764d6bec7c8fa7a407ac4db
Emre Küçükbaş <emre_efe.18@hotmail.com>
2025-12-16 23:36:54 +0300

    Go Web Scraper

---
+++ go.mod
@@ -0,0 +1,17 @@
+module url_detay
+
+go 1.25.5
+
+require (
+	github.com/PuerkitoBio/goquery v1.11.0 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/chromedp/cdproto v0.0.0-20250724212937-08a3db8b4327 // indirect
+	github.com/chromedp/chromedp v0.14.2 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/go-json-experiment/json v0.0.0-20250725192818-e39067aee2d2 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+)
---
+++ url_detay.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	urlFlag := flag.String("url", "", "Hedef web sitesi URL'si (zorunlu)")
+	flag.Parse()
+
+	if *urlFlag == "" {
+		log.Fatalf("Hata: URL belirtilmedi!\nKullanım: go run url_detay.go -url=google.com")
+	}
+
+	input := strings.TrimSpace(*urlFlag)
+
+	// Protokol yoksa ekle
+	if !strings.HasPrefix(strings.ToLower(input), "http://") &&
+		!strings.HasPrefix(strings.ToLower(input), "https://") {
+		input = "https://" + input
+	}
+
+	parsedURL, err := url.Parse(input)
+	if err != nil || parsedURL.Host == "" {
+		log.Fatalf("Geçersiz URL: %s", input)
+	}
+
+	targetURL := parsedURL.String()
+	fmt.Println("Hedef URL:", targetURL)
+
+	// Klasörler
+	os.MkdirAll("ekran_goruntusu", os.ModePerm)
+	os.MkdirAll("html", os.ModePerm)
+	os.MkdirAll("url_text", os.ModePerm)
+
+	// Güvenli dosya adı
+	safeName := parsedURL.Host
+	if parsedURL.Path != "" && parsedURL.Path != "/" {
+		safeName += strings.ReplaceAll(parsedURL.Path, "/", "_")
+	}
+
+	htmlPath := filepath.Join("html", safeName+".html")
+	screenPath := filepath.Join("ekran_goruntusu", safeName+".png")
+	urlPath := filepath.Join("url_text", safeName+".txt")
+
+	// Chrome ayarları
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Headless,
+		chromedp.DisableGPU,
+		chromedp.NoSandbox,
+	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	ctx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(ctx, 40*time.Second)
+	defer cancel()
+
+	var htmlContent string
+	var screenshot []byte
+
+	err = chromedp.Run(ctx,
+		chromedp.Navigate(targetURL),
+		chromedp.WaitVisible("body", chromedp.ByQuery),
+		chromedp.OuterHTML("html", &htmlContent),
+		chromedp.FullScreenshot(&screenshot, 90),
+	)
+
+	if err != nil {
+		log.Fatalf("Sayfa yüklenemedi: %v", err)
+	}
+
+	os.WriteFile(htmlPath, []byte(htmlContent), 0644)
+	os.WriteFile(screenPath, screenshot, 0644)
+
+	fmt.Println("✓ HTML:", htmlPath)
+	fmt.Println("✓ Screenshot:", screenPath)
+
+	// Link çıkarma
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	f, _ := os.Create(urlPath)
+	defer f.Close()
+
+	count := 0
+	doc.Find("a[href]").Each(func(i int, s *goquery.Selection) {
+		if href, ok := s.Attr("href"); ok {
+			fmt.Println(href)
+			io.WriteString(f, href+"\n")
+			count++
+		}
+	})
+
+	fmt.Printf("\nToplam %d link bulundu → %s\n", count, urlPath)
+	fmt.Println("İşlem tamamlandı ✔")
+}
